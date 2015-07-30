a5bcce8bc66103c22f36159dc4d7ab66879cd882
andrewariley87 <andrewariley87@gmail.com>
2015-07-30 10:44:03 -0400

    nightmare
    first stab at nightmare mode

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :body
   validates_presence_of :summary
+  validate :number_of_characters_short
+
+  def number_of_characters_short
+    number_of_characters_short = 10 - self.title.length
+    if self.title.length < 10
+    errors[:base] << "Your title is #{number_of_characters_short} characters short."
+    end
+  end
+
 
   belongs_to :author
 
